daced7ae6dbfdd32af727dca992588a4e0883d64
mbhmishu <mbhmishu@gmail.com>
2023-01-15 02:36:26 +0600

    intrview task

---
+++ src/product/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from config.g_model import TimeStampMixin
+
+
+# Create your models here.
+class Variant(TimeStampMixin):
+    title = models.CharField(max_length=40, unique=True)
+    description = models.TextField()
+    active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+
+class Product(TimeStampMixin):
+    title = models.CharField(max_length=255)
+    sku = models.SlugField(max_length=255, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-created',]
+
+
+
+class ProductImage(TimeStampMixin):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    file_path = models.URLField()
+
+
+class ProductVariant(TimeStampMixin):
+    variant_title = models.CharField(max_length=255)
+    variant = models.ForeignKey(Variant, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.variant_title
+
+
+class ProductVariantPrice(TimeStampMixin):
+    product_variant_one = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
+                                            related_name='product_variant_one')
+    product_variant_two = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
+                                            related_name='product_variant_two')
+    product_variant_three = models.ForeignKey(ProductVariant, on_delete=models.CASCADE, null=True,
+                                              related_name='product_variant_three')
+    price = models.FloatField()
+    stock = models.FloatField()
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'price {self.price} ::stock {self.stock}'
